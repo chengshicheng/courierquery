aac748116ecd9b751584a2e16c3fcf558c84a51d
chengshicheng <cheng221@outlook.com>
2017-02-11 00:39:48 +0800

    主界面列表绘制

---
+++ app/src/main/java/com/chengshicheng/courierquery/Activity/OnItemClickListener.java
@@ -0,0 +1,13 @@
+package com.chengshicheng.courierquery.Activity;
+
+import android.view.View;
+
+/**
+ * ItemClick的回调接口
+ * <p>
+ * Created by chengshicheng on 2017/2/11.
+ */
+
+public interface OnItemClickListener {
+    void onItemClick(View view, int position);
+}
